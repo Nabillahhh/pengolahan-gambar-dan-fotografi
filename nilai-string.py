3880a45ca6e7c2670c9218245e87b9e06ffaa008
Nabillahhh <nabillahputri89@gmail.com>
2024-01-03 19:38:10 +0700

    menambah file

---
+++ nilai-string.py
@@ -0,0 +1,8 @@
+# menggabungkan nilai dari string
+first_name = "Nabillah"
+last_name = "Aryanda"
+
+# ingin output menjadi "Nabillah Aryanda"
+print(first_name+last_name) #tanpa spasi
+print(first_name+" "+last_name) #menggunakan spasi
+print("Hello", first_name+" "+last_name)
